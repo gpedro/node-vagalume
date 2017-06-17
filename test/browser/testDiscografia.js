574b92e49e1675caf366efeb3e6c50d725b95d96
Gabriel Pedro <gpedro831@gmail.com>
2017-06-16 22:54:13 -0400

    fix tests

--- test/browser/testDiscografia.js
+++ test/browser/testDiscografia.js
@@ -5,7 +5,7 @@ var api = new Vagalume();
 
 test('VagalumeAPI :: getDiscografia(Rage Against the Machine)', function (t) {
   api.getDiscografia('Rage Against the Machine').then(function (response) {
-    t.equal(5, response.discography.item.length);
+    t.equal(6, response.discography.item.length);
     t.end();
   }).catch(function (err) {
     t.end(err);
